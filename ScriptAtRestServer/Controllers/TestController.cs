329f384ed2403c486acefdebf2b30f48710213a7
Michal <zezulami@gmail.com>
2021-03-19 21:00:29 +0100

    added test controller for authentication testing

---
+++ ScriptAtRestServer/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScriptAtRestServer.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("test")]
+    public class TestController : Controller
+    {
+        [HttpGet("authenticate")]
+        public IActionResult Test() {
+            return Ok(new { 
+                ExitCode = 0,
+                Output = "Ahoj"
+            });
+        }
+    }
+}
