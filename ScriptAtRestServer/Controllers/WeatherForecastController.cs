6579505725859496a2be3a6cf4d7d61caa38ce48
Michal <zezulami@gmail.com>
2021-03-19 20:43:52 +0100

    renamed controller to weather

--- ScriptAtRestServer/Controllers/WeatherForecastController.cs
+++ ScriptAtRestServer/Controllers/WeatherForecastController.cs
@@ -9,20 +9,13 @@ using System.Threading.Tasks;
 namespace ScriptAtRestServer.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("weather")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
 
         [Authorize]
         [HttpGet]
