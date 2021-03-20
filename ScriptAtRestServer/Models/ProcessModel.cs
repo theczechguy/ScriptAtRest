59458998b7fedad9b5789bcdc8c77bfe0da58c41
Michal <zezulami@gmail.com>
2021-03-20 10:26:23 +0100

    define process model for the output

---
+++ ScriptAtRestServer/Models/ProcessModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ScriptAtRestServer.Services
+{
+    public class ProcessModel
+    {
+        public Int32 ReturnCode { get; set; }
+        public string Output { get; set; }
+        public string ErrorOutput { get; set; }
+    }
+}
