1b957655ea13074099d9e63315c8770650735b6c
Michal <zezulami@gmail.com>
2021-03-20 10:56:09 +0100

    renamed returncode to exitcode

--- ScriptAtRestServer/Models/ProcessModel.cs
+++ ScriptAtRestServer/Models/ProcessModel.cs
@@ -4,7 +4,7 @@ namespace ScriptAtRestServer.Services
 {
     public class ProcessModel
     {
-        public Int32 ReturnCode { get; set; }
+        public Int32 ExitCode { get; set; }
         public string Output { get; set; }
         public string ErrorOutput { get; set; }
     }
