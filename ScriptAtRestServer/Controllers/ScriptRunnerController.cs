77b0d6e9f1709e434beac5e57db5a948c759a11d
Michal <zezulami@gmail.com>
2021-03-20 10:27:00 +0100

    created controller which executes script using injected scriptexecutionservice

---
+++ ScriptAtRestServer/Controllers/ScriptRunnerController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ScriptAtRestServer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScriptAtRestServer.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("script")]
+    public class ScriptRunnerController : Controller
+    {
+        private IScriptExecutionService _scriptExecutionService;
+
+        public ScriptRunnerController(IScriptExecutionService ScriptExecutionService) {
+            _scriptExecutionService = ScriptExecutionService;
+        }
+
+        [HttpGet("run/{Scriptname}")]
+        public async Task<IActionResult> ExecuteScript(string Scriptname) {
+            ProcessModel p = await _scriptExecutionService.RunScript(
+                ScriptEnums.ScriptType.PowerShell,
+                "Test.ps1",
+                string.Empty
+                );
+            return new ObjectResult(new
+            {
+                ReturnCode = p.ReturnCode,
+                Output = p.Output,
+                ErrorOutput = p.ErrorOutput
+            });
+        }
+    }
+}
