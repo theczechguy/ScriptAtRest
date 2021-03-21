6756f54762b7425fcf8195c45defb94ceedeb549
Michal <zezulami@gmail.com>
2021-03-21 14:52:23 +0100

    defined basic script service which is supposed to handle storage and related operations of scripts in database

---
+++ ScriptAtRestServer/Services/ScriptService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ScriptAtRestServer.Entities;
+using ScriptAtRestServer.Helpers;
+
+namespace ScriptAtRestServer.Services
+{
+
+    public interface IScriptService {
+        Script Create(string Name , string Content);
+    }
+    public class ScriptService : IScriptService
+    {
+        private SqLiteDataContext _context;
+        public ScriptService(SqLiteDataContext Context)
+        {
+            _context = Context;
+        }
+        public Script Create(string Name , string Content)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
