979fc44a057607afb38da72fdb4561cc7a04dde9
Michal <zezulami@gmail.com>
2021-03-21 10:22:44 +0100

    defined data context class for connection to sqlite DB

---
+++ ScriptAtRestServer/Controllers/SqLiteDataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using ScriptAtRestServer.Entities;
+
+namespace ScriptAtRestServer.Controllers
+{
+    public class SqLiteDataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public SqLiteDataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sql server database
+            options.UseSqlite(Configuration.GetConnectionString("SqLiteDatabase"));
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
