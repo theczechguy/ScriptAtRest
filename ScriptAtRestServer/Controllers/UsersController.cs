cae199eeee85934d991f67c5df58c88e6049af6d
Michal <zezulami@gmail.com>
2021-03-21 11:16:38 +0100

    create new controller for user operations

---
+++ ScriptAtRestServer/Controllers/UsersController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using ScriptAtRestServer.Helpers;
+using Microsoft.Extensions.Options;
+using System.Text;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using ScriptAtRestServer.Services;
+using ScriptAtRestServer.Entities;
+using ScriptAtRestServer.Models.Users;
+
+namespace ScriptAtRestServer.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("users")]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+        private IMapper _mapper;
+
+        public UsersController(
+            IUserService userService,
+            IMapper mapper)
+        {
+            _userService = userService;
+            _mapper = mapper;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public IActionResult Register([FromBody]RegisterModel model)
+        {
+            // map model to entity
+            var user = _mapper.Map<User>(model);
+
+            try
+            {
+                // create user
+                _userService.Create(user, model.Password);
+                return Ok();
+            }
+            catch (AppException ex)
+            {
+                // return error message if there was an exception
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+            var model = _mapper.Map<IList<UserModel>>(users);
+            return Ok(model);
+        }
+    }
+}
