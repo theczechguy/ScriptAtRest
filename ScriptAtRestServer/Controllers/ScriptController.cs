7c65414f1655ae84a14a1c30ebd5c330bb25dce3
Michal <zezulami@gmail.com>
2021-03-23 17:40:01 +0100

    added missing logs

--- ScriptAtRestServer/Controllers/ScriptController.cs
+++ ScriptAtRestServer/Controllers/ScriptController.cs
@@ -40,50 +40,56 @@ namespace ScriptAtRestServer.Controllers
 
         [HttpPost("register")]
         public IActionResult Register([FromBody] RegisterScriptModel model) {
+            _logger.LogInformation("Register new script");
+            _logger.LogDebug("{@registerScriptModel}" , model);
             Script script = _mapper.Map<Script>(model);
             try
             {
-                _scriptService.Create(script);
+                Script createdScript = _scriptService.Create(script);
+                _logger.LogInformation("Script registered with id : @{scriptId}" , createdScript.Id);
                 return Ok();
             }
             catch (AppException ex)
             {
+                _logger.LogError(ex , "Failed to register new script");
                 return BadRequest(new { message = ex.Message });
             }
         }
 
         [HttpGet]
         public IActionResult GetAll() {
+            _logger.LogInformation("Get all scripts");
             var scripts = _scriptService.GetAll();
             var model = _mapper.Map<IList<ScriptModel>>(scripts);
+            _logger.LogDebug("Scripts retrieved : @{scriptCount}" , model.Count);
             return Ok(model);
         }
 
         [HttpGet("{id}")]
         public IActionResult GetById(int Id) {
+            _logger.LogInformation("Get Script with id : @{scriptid}" , Id);
             var script = _scriptService.GetById(Id);
             var model = _mapper.Map<ScriptModel>(script);
+            _logger.LogDebug("Retrieved script : @{script}" , script);
             return Ok(model);
         }
 
         [HttpDelete("{id}")]
         public IActionResult Delete(int Id) {
+            _logger.LogInformation("Delete script with ID : @{scriptid}" , Id);
             _scriptService.Delete(Id);
             return Ok();
         }
 
         [HttpPost("run/{id}")]
-        public async Task<IActionResult> ExecuteScriptById(int id)
+        public async Task<IActionResult> ExecuteScriptById(int Id)
         {
-            _logger.LogInformation("New request for: Run Script by ID");
-            ProcessModel p = await _scriptExecutionService.RunScriptById(id);
+            _logger.LogInformation("Run script with ID : @{scriptid}" , Id);
+            ProcessModel p = await _scriptExecutionService.RunScriptById(Id);
             _logger.LogInformation("Script exit code : {ExitCode}", p.ExitCode);
-            return new ObjectResult(new
-            {
-                ExitCode = p.ExitCode,
-                Output = p.Output,
-                ErrorOutput = p.ErrorOutput
-            });
+            _logger.LogDebug("Script result : @{processModel}" , p);
+
+            return Ok(p);
         }
     }
 }
