b932ffa51e8be2e4f6f34aa694c0b57d55aa7f72
Michal <zezulami@gmail.com>
2021-03-29 19:41:15 +0200

    define new model for script parameters

---
+++ ScriptAtRestServer/Models/Scripts/ScriptParamModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ScriptAtRestServer.Models.Scripts
+{
+    public class ScriptParamModel
+    {
+        public string ParameterName { get; set; }
+        public string ParameterValue { get; set; }
+    }
+
+    public class ScriptParamArray { 
+        public List<ScriptParamModel> Parameters { get; set; }
+    }
+}
