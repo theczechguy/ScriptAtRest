acb75b111e1c905e1a245a00c4413660e993f751
Michal <zezulami@gmail.com>
2021-03-30 18:53:23 +0200

    separated classes into individual files

--- ScriptAtRestServer/Models/Scripts/ScriptParamModel.cs
+++ ScriptAtRestServer/Models/Scripts/ScriptParamModel.cs
@@ -1,14 +1,8 @@
-﻿using System.Collections.Generic;
-
-namespace ScriptAtRestServer.Models.Scripts
+﻿namespace ScriptAtRestServer.Models.Scripts
 {
     public class ScriptParamModel
     {
         public string ParameterName { get; set; }
         public string ParameterValue { get; set; }
-    }
-
-    public class ScriptParamArray { 
-        public List<ScriptParamModel> Parameters { get; set; }
     }
 }
