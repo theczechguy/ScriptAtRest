40835dd8e34cc6aeaaba1ceec5a0c3778cf45698
Michal <zezulami@gmail.com>
2021-03-31 15:47:41 +0200

    make both script param fields required

--- ScriptAtRestServer/Models/Scripts/ScriptParamModel.cs
+++ ScriptAtRestServer/Models/Scripts/ScriptParamModel.cs
@@ -1,8 +1,12 @@
-﻿namespace ScriptAtRestServer.Models.Scripts
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ScriptAtRestServer.Models.Scripts
 {
     public class ScriptParamModel
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string EncodedValue { get; set; }
     }
 }
