8754644f63d7955e661ffecd489856eef7b66807
Michal <zezulami@gmail.com>
2021-04-02 13:23:29 +0200

    switch to debug by default

--- Tests/StartUserControllerTest.ps1
+++ Tests/StartUserControllerTest.ps1
@@ -1,5 +1,5 @@
 $pesterContainer = New-PesterContainer -Path .\UserController.Tests.ps1 -Data @{
-    ApplicationFolder = 'c:\Users\alien\OneDrive\Dokumenty\GitHub\ScriptAtRest\ScriptAtRestServer\bin\Release\netcoreapp3.1\'
+    ApplicationFolder = 'c:\Users\alien\OneDrive\Dokumenty\GitHub\ScriptAtRest\ScriptAtRestServer\bin\Debug\netcoreapp3.1\'
     Username = "Test{0}" -f (Get-Random -Minimum 1 -Maximum 10)
     UserPassword = 'test'
     ApiUrl = 'http://localhost:5000'
