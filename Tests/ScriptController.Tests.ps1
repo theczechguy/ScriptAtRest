b800b4a0d8612cdcf2e45c589960174f14e0f01d
Michal <zezulami@gmail.com>
2021-04-03 17:51:43 +0200

    test script registration

--- Tests/ScriptController.Tests.ps1
+++ Tests/ScriptController.Tests.ps1
@@ -2,7 +2,8 @@ param (
     $ApplicationFolder,
     $Username,
     $UserPassword,
-    $apiUrl
+    $apiUrl,
+    $ScriptName
 )
 BeforeAll {
     $exeFile = Join-Path -Path $ApplicationFolder -ChildPath "ScriptAtRestServer.exe"
@@ -10,4 +11,40 @@ BeforeAll {
     
     $process = Start-Process -FilePath $exeFile -WorkingDirectory $ApplicationFolder -PassThru
     Start-Sleep -Seconds 5
+
+    $body = @{
+        "firstname" = "Test"
+        "lastname" = "Test"
+        "username" = $Username
+        "password" = $UserPassword
+    } | ConvertTo-Json
+
+    $response = Invoke-RestMethod `
+        -Method Post `
+        -Uri "$apiUrl/users/register" `
+        -Body $body `
+        -ContentType "application/json"
+}
+
+Describe "Script controller tests" {
+    It "Register a new script" {
+        $script = Get-Content .\TestScript.ps1
+
+        $Bytes = [System.Text.Encoding]::UTF8.GetBytes($script)
+        $encoded =[Convert]::ToBase64String($Bytes)
+
+        $body = @{
+            "name" = $ScriptName
+            "EncodedContent" = $encoded
+            "type" = 1
+        }
+
+        $response = Invoke-RestMethod `
+            -Method Post `
+            -Uri "$apiUrl/scripts/register" `
+            -Body $body `
+            -ContentType "application/json"
+        
+        $response.name | should -BeExactly $ScriptName
+    }
 }
