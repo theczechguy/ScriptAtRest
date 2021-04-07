95d1a5863349a9126d50473878a78be7f9451ac3
Michal <zezulami@gmail.com>
2021-04-07 17:31:33 +0200

    defined new entity - script type

---
+++ ScriptAtRestServer/Entities/ScriptType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScriptAtRestServer.Entities
+{
+    public class ScriptType
+    {
+        public string TypeName { get; set; }
+        public string Runner { get; set; }
+        public string FileExtension { get; set; }
+        public string FileParameter { get; set; }
+
+    }
+}
