83fcb0110d72db998ecedde7496dcd1fe5cef283
Michal <zezulami@gmail.com>
2021-04-07 17:47:54 +0200

    defined model which represents the script type

---
+++ ScriptAtRestServer/Models/ScriptTypes/ScriptTypeModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ScriptAtRestServer.Models.ScriptTypes
+{
+    public class ScriptTypeModel
+    {
+        public string Name { get; set; }
+
+        public string Runner { get; set; }
+
+        public string FileExtension { get; set; }
+
+        public string ScriptArgument { get; set; }
+    }
+}
