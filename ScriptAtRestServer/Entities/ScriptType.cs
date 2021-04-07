2907e1cb9d1f6dfd7e42ad49b74615d6f409909b
Michal <zezulami@gmail.com>
2021-04-07 17:42:17 +0200

    shorten the field name

--- ScriptAtRestServer/Entities/ScriptType.cs
+++ ScriptAtRestServer/Entities/ScriptType.cs
@@ -7,7 +7,7 @@ namespace ScriptAtRestServer.Entities
 {
     public class ScriptType
     {
-        public string TypeName { get; set; }
+        public string Name { get; set; }
         public string Runner { get; set; }
         public string FileExtension { get; set; }
         public string ScriptArgument { get; set; }
